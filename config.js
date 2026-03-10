977e62d511ff4a5ad93addc93432eadf7d775f9e
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2026-03-10 20:46:28 +0300

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+        * ==========================================
+        * CONFIGURATION
+        * ==========================================
+        * Edit these variables to setup a new project.
+        */
+const CONFIG = {
+    bookName: "testing",
+    totalChapters: 5,
+};
